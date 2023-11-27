ec9b2bbf3f40e7d2a8e09f44a278ff95778c6bf7
Belots-Belots <gustavobmluiz@hotmail.com>
2023-11-27 19:15:18 -0300

    alegria
    alegre

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,22 @@
 const TITLE = "Digital-Wallet";
 console.log(`[${TITLE}] init`);
+
+function calculate() {
+  const personalMoney = document.getElementById("personal-money")
+  console.log(`[${TITLE}] personalMoney`, personalMoney) 
+  console.log(`[${TITLE}] personalMoney.value`, personalMoney.value) 
+  
+  const livingMoney = document.getElementById("living-money")
+  console.log(`[${TITLE}] livingMoney`, livingMoney) 
+  console.log(`[${TITLE}] livingMoney.value`, livingMoney.value) 
+
+  const extraMoney = document.getElementById("extra-money")
+  console.log(`[${TITLE}] extraMoney`, extraMoney) 
+  console.log(`[${TITLE}] extraMoney.value`, extraMoney.value)
+
+  const total = document.getElementById("total")
+  console.log(`[${TITLE}] total`, total) 
+  console.log(`[${TITLE}] total.value`, total.value) 
+ 
+  total.value = parseInt(personalMoney.value) + parseInt(livingMoney.value) + parseInt(extraMoney.value)
+}
