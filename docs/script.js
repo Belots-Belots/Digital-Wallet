2ff347bd75cc3a59dc8f61f7f7ade048c7c8f363
dudushy <eduardotalarico1337@gmail.com>
2023-11-27 23:40:14 -0300

    Working on frontend

--- docs/script.js
+++ docs/script.js
@@ -3,20 +3,28 @@ console.log(`[${TITLE}] init`);
 
 function calculate() {
   const personalMoney = document.getElementById("personal-money")
-  console.log(`[${TITLE}] personalMoney`, personalMoney) 
-  console.log(`[${TITLE}] personalMoney.value`, personalMoney.value) 
-  
+  console.log(`[${TITLE}] personalMoney`, personalMoney)
+  console.log(`[${TITLE}] personalMoney.value`, personalMoney.value)
+
   const livingMoney = document.getElementById("living-money")
-  console.log(`[${TITLE}] livingMoney`, livingMoney) 
-  console.log(`[${TITLE}] livingMoney.value`, livingMoney.value) 
+  console.log(`[${TITLE}] livingMoney`, livingMoney)
+  console.log(`[${TITLE}] livingMoney.value`, livingMoney.value)
 
   const extraMoney = document.getElementById("extra-money")
-  console.log(`[${TITLE}] extraMoney`, extraMoney) 
+  console.log(`[${TITLE}] extraMoney`, extraMoney)
   console.log(`[${TITLE}] extraMoney.value`, extraMoney.value)
 
   const total = document.getElementById("total")
-  console.log(`[${TITLE}] total`, total) 
-  console.log(`[${TITLE}] total.value`, total.value) 
- 
+  console.log(`[${TITLE}] total`, total)
+  console.log(`[${TITLE}] total.value`, total.value)
+
   total.value = parseInt(personalMoney.value) + parseInt(livingMoney.value) + parseInt(extraMoney.value)
+
+  if (total.classList.contains("flip")) {
+    total.classList.remove("flip")
+    total.classList.add("flip-alt")
+  } else {
+    total.classList.remove("flip-alt")
+    total.classList.add("flip")
+  }
 }
